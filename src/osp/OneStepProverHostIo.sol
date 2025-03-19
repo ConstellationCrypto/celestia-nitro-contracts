6c1a4d8e5f3e277a879fee6cc2ac3d1b705a8e74
Ino Murko <2582555+InoMurko@users.noreply.github.com>
2025-03-19 17:25:21 +0100

    celestia 2.1.3 arb1

--- src/osp/OneStepProverHostIo.sol
+++ src/osp/OneStepProverHostIo.sol
@@ -43,10 +43,10 @@ contract OneStepProverHostIo is IOneStepProver {
 
     //Base
     //Arb
-    //address public constant BLOBSTREAM = 0xA83ca7775Bc2889825BcDeDfFa5b758cf69e8794;
+    address public constant BLOBSTREAM = 0xA83ca7775Bc2889825BcDeDfFa5b758cf69e8794;
     
     //Sepolia 
-    address public constant BLOBSTREAM = 0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e;
+    //address public constant BLOBSTREAM = 0xF0c6429ebAB2e7DC6e05DaFB61128bE21f13cb1e;
 
 
     //Mainnet
